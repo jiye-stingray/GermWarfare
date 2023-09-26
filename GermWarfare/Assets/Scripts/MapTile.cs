8d65b3b0c4fafcfe8c3408f8124f2803f27c807a
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2023-09-26 20:24:38 +0900

    :sparkles: ㅣ MapTile & Germ Prefab

---
+++ GermWarfare/Assets/Scripts/MapTile.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GermType
+{
+    None,
+    Red,
+    Blue
+}
+
+public class MapTile : MonoBehaviour
+{
+    public GermType _mapGermType;          // 현재 map에 type 상태
+
+}
