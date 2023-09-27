be96d70236e8326077d5eab83aefcaf4d2876457
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2023-09-27 10:29:00 +0900

    :sparkles: ㅣ current type UI

---
+++ GermWarfare/Assets/Scripts/UI/CurrentTypeUI.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CurrentTypeUI : MonoBehaviour
+{
+    [SerializeField] private Image[] _currentTypeImg;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < _currentTypeImg.Length; i++)
+        {
+            _currentTypeImg[i].color = (InGameManager.Instance._currentType == GermType.Blue) ? Color.blue : Color.red;
+        }
+    }
+}
