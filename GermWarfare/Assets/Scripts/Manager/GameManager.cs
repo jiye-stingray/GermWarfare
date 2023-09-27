38d448ae317f4127bcde6673eb09fa9b881a634c
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2023-09-27 23:07:35 +0900

    🤩 ㅣ GameOver Panel

--- GermWarfare/Assets/Scripts/Manager/GameManager.cs
+++ GermWarfare/Assets/Scripts/Manager/GameManager.cs
@@ -51,6 +51,7 @@ public class GameManager : Singleton<GameManager>
     public void GameOver()
     {
         Debug.Log("GameOver");
+        GameOverUI.Instance.GameOver();
     }
 
 
---
+++ GermWarfare/Assets/Scripts/UI/GameOverUI.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameOverUI : Singleton<GameOverUI>
+{
+    [SerializeField] GameObject _gameOverPanelObj;
+    [SerializeField] TMP_Text _winText;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _gameOverPanelObj.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GameOver()
+    {
+        _gameOverPanelObj.SetActive(true);
+        _winText.text = GameManager.Instance.ScoreBlue > GameManager.Instance.ScoreRed ? "Blue" : "Red";
+        _winText.text += " Win!";
+    }
+
+    public void ReStartBtnClickEvent()
+    {
+        SceneManager.LoadScene("InGameScene");
+    }
+
+    public void MenuBtnClickEvent()
+    {
+        // MenuSceneLoad
+    }
+}
