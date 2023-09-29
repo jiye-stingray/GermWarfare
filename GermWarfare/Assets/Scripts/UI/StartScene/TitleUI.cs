f37e17fdb71667dc0a0d2f2ab659c747122d9a1d
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2023-09-29 23:10:39 +0900

    :sparkles: ㅣ Back Btn

--- GermWarfare/Assets/Scripts/UI/StartScene/TitleUI.cs
+++ GermWarfare/Assets/Scripts/UI/StartScene/TitleUI.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class TitleUI : MonoBehaviour
 {
     [SerializeField] GameObject _normalMapInputPanle;
+    [SerializeField] GameObject _customMapInputPanel;
 
     // Start is called before the first frame update
     void Start()
     {
         _normalMapInputPanle.SetActive(false);
+        _customMapInputPanel.SetActive(false);
     }
 
     // Update is called once per frame
@@ -21,6 +23,17 @@ public class TitleUI : MonoBehaviour
     public void NormalMapBtnClickEvent()
     {
         _normalMapInputPanle.SetActive(true);
+    }
+    public void CustomMapBtnClickEvent()
+    {
+        _customMapInputPanel.SetActive(true);
+    }
+
+    // 돌아가기 버튼
+    public void BackBtnClickEvent()
+    {
+        _normalMapInputPanle?.SetActive(false);
+        _customMapInputPanel?.SetActive(false);
     }
 
 }
