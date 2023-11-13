a2a117497a73f96a8a67574950cb8819603af587
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2023-11-13 16:01:02 +0900

    :sparkles: ㅣ Create Banner and Load Ad

---
+++ GermWarfareMobile/Assets/Scripts/UI/Addmob/AddmobBanner.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+public class AddmobBanner : MonoBehaviour
+{
+
+    // These ad units are configured to always serve test ads.
+#if UNITY_ANDROID
+    private string _adUnitId = "ca-app-pub-3940256099942544/6300978111";
+#elif UNITY_IPHONE
+  private string _adUnitId = "ca-app-pub-3940256099942544/2934735716";
+#else
+  private string _adUnitId = "unused";
+#endif
+
+    private BannerView _bannerView;
+
+    public void Start()
+    {
+        MobileAds.Initialize((InitializationStatus initStatus) =>
+        {
+            // This callback is called once the MobileAds SDK is initialized.
+        });
+
+        CreateBannerView();
+    }
+
+    /// <summary>
+    /// Creates a 320x50 banner view at top of the screen.
+    /// </summary>
+    public void CreateBannerView()
+    {
+        Debug.Log("Creating banner view");
+
+
+        // Create a 320x50 banner at top of the screen
+        _bannerView = new BannerView(_adUnitId, AdSize.Banner, AdPosition.BottomRight);
+
+        LoadAd();
+    }
+
+
+    /// <summary>
+    /// Creates the banner view and loads a banner ad.
+    /// </summary>
+    public void LoadAd()
+    {
+        // create an instance of a banner view first.
+        if (_bannerView == null)
+        {
+            CreateBannerView();
+        }
+
+        // create our request used to load the ad.
+        var adRequest = new AdRequest();
+
+        // send the request to load the ad.
+        Debug.Log("Loading banner ad.");
+        _bannerView.LoadAd(adRequest);
+    }
+}
