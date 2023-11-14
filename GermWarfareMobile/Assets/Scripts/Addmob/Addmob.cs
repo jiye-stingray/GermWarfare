b1261fcbce236d3557557e05ff83e1e6d68d20a1
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2023-11-14 14:07:56 +0900

    :art: l Title UI

--- GermWarfareMobile/Assets/Scripts/Addmob/Addmob.cs
+++ GermWarfareMobile/Assets/Scripts/Addmob/Addmob.cs
@@ -5,14 +5,7 @@ using UnityEngine;
 
 public class Addmob : MonoBehaviour
 {
-    // These ad units are configured to always serve test ads.
-#if UNITY_ANDROID
-    protected string _adUnitId = "ca-app-pub-3940256099942544/6300978111";          //temp
-#elif UNITY_IPHONE
-  protected string _adUnitId = "ca-app-pub-3940256099942544/2934735716";
-#else
-  protected string _adUnitId = "unused";
-#endif
+
 
     protected void Start()
     {
@@ -21,18 +14,6 @@ public class Addmob : MonoBehaviour
             // This callback is called once the MobileAds SDK is initialized.
         });
 
-    }
-
-    protected void InitId(string custId)
-    {
-
-#if UNITY_ANDROID
-        _adUnitId = custId;          
-#elif UNITY_IPHONE
-  _adUnitId = "ca-app-pub-3940256099942544/2934735716";
-#else
-  _adUnitId = "unused";
-#endif
     }
 
 }
